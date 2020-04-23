82ca1d2e6a1e46d7aedb60adca2804f5f19463f2
kiranKamath <k96white@gmail.com>
2020-04-23 20:27:09 +0530

    Updates

---
+++ precache-manifest.63748602f7c979054bea639c0952d99e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd1a373b3780e633856e0085975ea3d",
+    "url": "/recipeApp-redux/index.html"
+  },
+  {
+    "revision": "90208ece9d3af0f543b1",
+    "url": "/recipeApp-redux/static/css/main.1bc4f461.chunk.css"
+  },
+  {
+    "revision": "233e2a67e3964aaf70f3",
+    "url": "/recipeApp-redux/static/js/2.db1f8555.chunk.js"
+  },
+  {
+    "revision": "90208ece9d3af0f543b1",
+    "url": "/recipeApp-redux/static/js/main.0c16b563.chunk.js"
+  },
+  {
+    "revision": "e8ca13cf83ddad80819d",
+    "url": "/recipeApp-redux/static/js/runtime~main.ac42dab7.js"
+  }
+]);
